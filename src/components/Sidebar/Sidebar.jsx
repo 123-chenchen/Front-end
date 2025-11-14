08201b89c8c1e65221778e3b432093d02216d72d
Thao <phamphuongthao2792004@gmail.com>
2025-11-15 00:31:47 +0700

    fix logo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,14 @@
 import React, { useEffect } from 'react';
-import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  ListItemIcon,
+  Box,
+  CircularProgress,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,8 +18,11 @@ import { useGetGenresQuery } from '../../services/TMDB';
 import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import genreIcons from '../../assets/genres';
 
-const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
-const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
+// ⚠️ SỬA ĐƯỜNG DẪN LOGO THÀNH assets
+// Giả sử file nằm ở: src/assets/logo/Filmpire.jpg
+import redLogo from '../../assets/logo/Filmpire.jpg';
+import blueLogo from '../../assets/logo/Filmpire.jpg';
+// Nếu sau này bạn có 2 file khác nhau, chỉ cần đổi tên file và đường dẫn là được
 
 const categories = [
   { label: 'Popular', value: 'popular' },
@@ -23,53 +35,82 @@ function Sidebar({ setMobileOpen }) {
   const sx = styles(theme);
   const dispatch = useDispatch();
   const { data, isFetching } = useGetGenresQuery();
-  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
+  const { genreIdOrCategoryName } = useSelector(
+    (state) => state.currentGenreOrCategory,
+  );
 
   useEffect(() => {
+    // Đóng sidebar mobile khi đổi category / genre
     setMobileOpen(false);
-  }, [genreIdOrCategoryName]);
+  }, [genreIdOrCategoryName, setMobileOpen]);
+
+  const logo = theme.palette.mode === 'light' ? redLogo : blueLogo;
 
   return (
     <>
+      {/* Logo */}
       <Link to="/" style={sx.imageLink}>
         <img
           style={sx.image}
-          src={theme.palette.mode === 'light' ? redLogo : blueLogo}
+          src={logo}
           alt="Filmpire Logo"
         />
       </Link>
+
       <Divider />
+
+      {/* Categories */}
       <List>
         <ListSubheader>Categories</ListSubheader>
         {categories.map(({ label, value }) => (
           <Link key={value} style={sx.links} to="/">
-            <ListItem button onClick={() => dispatch(selectGenreOrCategory(value))}>
+            <ListItem
+              button
+              onClick={() => dispatch(selectGenreOrCategory(value))}
+            >
               <ListItemIcon>
-                <img src={genreIcons[label.toLowerCase()]} style={sx.genreImages} height={30} />
+                <img
+                  src={genreIcons[label.toLowerCase()]}
+                  style={sx.genreImages}
+                  height={30}
+                  alt={label}
+                />
               </ListItemIcon>
               <ListItemText primary={label} />
             </ListItem>
           </Link>
         ))}
       </List>
+
       <Divider />
+
+      {/* Genres */}
       <List>
         <ListSubheader>Genres</ListSubheader>
         {isFetching ? (
           <Box display="flex" justifyContent="center">
             <CircularProgress size="4rem" />
           </Box>
-        )
-          : data?.genres?.map(({ name, id }) => (
+        ) : (
+          data?.genres?.map(({ name, id }) => (
             <Link key={name} style={sx.links} to="/">
-              <ListItem button onClick={() => dispatch(selectGenreOrCategory(id))}>
+              <ListItem
+                button
+                onClick={() => dispatch(selectGenreOrCategory(id))}
+              >
                 <ListItemIcon>
-                  <img src={genreIcons[name.toLowerCase()]} style={sx.genreImages} height={30} />
+                  <img
+                    src={genreIcons[name.toLowerCase()]}
+                    style={sx.genreImages}
+                    height={30}
+                    alt={name}
+                  />
                 </ListItemIcon>
                 <ListItemText primary={name} />
               </ListItem>
             </Link>
-          ))}
+          ))
+        )}
       </List>
     </>
   );
